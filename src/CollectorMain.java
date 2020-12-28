58817d9f03b29e68aa973fdbc2f937091b702ec1
pklemettila <pauli.klemettila@gmail.com>
2020-12-28 21:28:55 +0200

    first commit

---
+++ src/CollectorMain.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CollectorMain {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JButton b = new JButton("Click here");
+        b.setBounds(50, 50, 200, 40);
+        final JLabel tf = new JLabel();
+        tf.setBounds(75, 150, 200, 30);
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("Well clicked my friend!");
+            }
+        });
+
+
+        f.add(b);
+        f.add(tf);
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
+
