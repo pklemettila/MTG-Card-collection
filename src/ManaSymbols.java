a699d9b45157b71d4694763838718337363d224b
pklemettila <pauli.klemettila@gmail.com>
2020-12-29 23:54:36 +0200

    Started on manasymbols

---
+++ src/ManaSymbols.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class ManaSymbols extends JPanel {
+
+    private JToggleButton blue = new JToggleButton();
+    private JToggleButton black = new JToggleButton();
+    private JToggleButton green = new JToggleButton();
+    private JToggleButton red = new JToggleButton();
+    private JToggleButton white = new JToggleButton();
+    private JToggleButton multicoloured = new JToggleButton();
+    private JToggleButton colourless = new JToggleButton();
+
+    public ManaSymbols() {
+
+
+
+    }
+}
