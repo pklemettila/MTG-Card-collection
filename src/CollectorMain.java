128c408250b14b5f9b7230a98d0e33353af4c322
Niina Lunden <niina.lunden@student.oulu.fi>
2021-01-07 00:20:40 +0200

    matti

--- src/CollectorMain.java
+++ src/CollectorMain.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class CollectorMain {
 
@@ -16,20 +17,23 @@ public class CollectorMain {
     public static void main(String[] args) {
         MainInterface minterface = new MainInterface();
         Database app = new Database();
-        //app.selectAllCreatureCards();
-        //System.out.println("by color: ");
-        //app.getCardColor("U");
-        //app.getArtist("Ittoku");
-        //app.updatePrice(15, "rare", "Christopher Rush");
         //app.getPrice(15);
         //app.substractFromOwned(3);
         //app.addToOwned(2543);
         //app.getOwnedCards();
-        //app.searchAll(false, false, false, false, false, false, true, false, false, false, false);
+        ArrayList<Integer> matti = new ArrayList<>();
+        matti = app.searchAll(
+            false, false, false, false, false, false, false, 
+            false, false, false, false,
+            false, 0, false, 0,
+            false,
+            false, "Creature",
+            false, false, false, false, "false");
         int num = app.searchOwnedById(3);
         System.out.println(num);
-
-
+        System.out.println(matti);
+        String pirkko = app.searchCardsById(53);
+System.out.println(pirkko);
 
     }
 
