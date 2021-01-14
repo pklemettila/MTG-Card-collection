fca36b8254bb60cfd42597dabebc1082d7d962d4
pklemettila <pauli.klemettila@gmail.com>
2021-01-14 02:29:03 +0200

    fixed search button

--- src/SearchBtn.java
+++ src/SearchBtn.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 public class SearchBtn extends JButton {
 
 
-    public void searchBtnImage(JButton searchButton){
+    public  SearchBtn(){
         ImageIcon searchImgIcon = new ImageIcon(this.getClass().getResource("/images/maglass.png"));
-        searchButton.setIcon(searchImgIcon);
+        this.setIcon(searchImgIcon);
     }
 }
