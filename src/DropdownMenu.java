f1130d63d6e9ed661a3e0b88b1c392c33fcba177
pklemettila <pauli.klemettila@gmail.com>
2021-01-17 01:10:33 +0200

    fixed dropdown error

--- src/DropdownMenu.java
+++ src/DropdownMenu.java
@@ -7,7 +7,7 @@ public class DropdownMenu extends JPanel {
 static final long serialVersionUID = 42L;
 
     private String[] s = {"Select Card Type", "Artifact", "Conspiracy", "Creature", "Enchantment", "Instant", "Land", "Planeswalker", "Sorcery"};
-    private JComboBox dropDown = new JComboBox(s);
+    private JComboBox<String> dropDown = new JComboBox<>(s);
 
 
     public DropdownMenu() {
